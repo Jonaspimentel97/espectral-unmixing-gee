109df951614584af1cfa26479ee92eb795d15b51
Jonaspimentel97 <jonaspimentel97@gmail.com>
2025-04-07 22:13:56 -0300

    Update mistura.py

--- mistura.py
+++ mistura.py
@@ -207,7 +207,8 @@ with st.sidebar:
     # Exibir imagens adicionais
     st.image("https://github.com/Jonaspimentel97/espectral-unmixing-gee/blob/main/2.png?raw=true", caption="Percentual de Cada Componente com Base no Modelo de Fração.", use_container_width=True)
     st.image("https://github.com/Jonaspimentel97/espectral-unmixing-gee/blob/main/3.png?raw=true", caption="Comparação entre MNDWI (A), NDWI (B) e Imagem de Frações (C)", use_container_width=True)
-    # Separador visual
+   
+      # Separador visual
     st.divider()
 
     # Título da próxima seção
